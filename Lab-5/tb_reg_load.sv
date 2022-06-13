9c1be1c03d4255b3e30df627655fe93886d085d5
Bruce Xi <91307696+brucexi999@users.noreply.github.com>
2022-06-13 11:41:54 -0700

    added reg_load and tb_reg_load

---
+++ Lab-5/tb_reg_load.sv
@@ -0,0 +1,22 @@
+module tb_reg_load.sv ();
+	logic [15:0] a, b;
+	logic load, clk;
+	
+	reg_load # (16) dut (.a (a), .b (b), .load (load), .clk (clk));
+	
+	initial begin
+		clk = 0; #5;
+		forever begin
+			clk = 1; #5;
+			clk = 0; #5;
+		end
+	end
+	
+	initial begin
+		load = 0; a = 'h0001; #5;
+		load = 1; #5;
+		load = 0; #5;
+		a = 'h0002; load = 1; #5;
+		load = 0; #5;
+	end
+endmodule
