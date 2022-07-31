c81de61590dfbc9d00fda8f7b9e763efc94051af
brucexi999 <91307696+brucexi999@users.noreply.github.com>
2022-07-30 18:33:58 -0700

    Update lab7_top.sv

--- Lab-7/lab7_top.sv
+++ Lab-7/lab7_top.sv
@@ -4,18 +4,38 @@ module lab7_top (KEY,SW,LEDR,HEX0,HEX1,HEX2,HEX3,HEX4,HEX5);
     output [9:0] LEDR;
     output [6:0] HEX0, HEX1, HEX2, HEX3, HEX4, HEX5;
 
-logic [1:0] mem_cmd; 
-logic [8:0] mem_addr; 
-logic [15:0] dout, read_data; 
-// Memory control unit.
-Tristate tri_drv (
-    .in (dout)
-    .out (read_data)
-    .enable ((mem_cmd == 2'b01 && mem_addr[8] == 1'b0))
-);
+    logic [1:0] mem_cmd; 
+    logic [8:0] mem_addr; 
+    logic [15:0] dout, read_data; 
 
-CPU cpu ();
-RAM ram (); 
+    // Memory control unit.
+    Tristate tri_drv (
+        .in (dout)
+        .out (read_data)
+        .enable ((mem_cmd == 2'b01 && mem_addr[8] == 1'b0))
+    );
+
+    CPU cpu (
+        .clk (),
+        .reset (),
+        .in (read_data),
+        .out (),
+        .N (), 
+        .V (), 
+        .Z (),
+        .w (),
+        .mem_cmd (mem_cmd),
+        .mem_addr (mem_addr)
+    );
+
+    RAM ram (
+        .clk (),
+        .read_address (mem_addr[7:0]),
+        .write_address (mem_addr[7:0]),
+        .write (),
+        .din (),
+        .dout (dout)
+    ); 
 
 
 endmodule
